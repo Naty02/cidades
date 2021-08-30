ad64449b7d92570864090050afe17346aa2b5746
Natalie Gonçalves <nataliegoncalvesdasilva02@gmail.com>
2021-08-30 00:57:58 -0300

    resolução dos exercício cidades

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
